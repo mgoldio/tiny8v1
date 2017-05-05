e785c1ec4fc8b878f91c4fec9005304e7f7c7618
Michael Goldstein <mjgolds2@illinois.edu>
2017-05-05 00:27:18 -0500

    datapath doneish

--- src/datapath.sv
+++ src/datapath.sv
@@ -14,69 +14,128 @@ module datapath
     input tiny8_aluop aluop,
 
     /* mux selects */
+    input pcmux_sel,
+    input [1:0] addrmux_sel,
+    input alumux1_sel,
+    input alumux2_sel,
+    input regfilemux_sel
 );
 
-tiny8_word pc_in;
 tiny8_word pc_out;
+tiny8_word ir_out;
+tiny8_word acc_out;
+tiny8_word rs_out;
+tiny8_word rd_out;
+tiny8_word alu_out;
+tiny8_word pcmux_out;
+tiny8_word addrmux_out;
+tiny8_word alumux1_out;
+tiny8_word alumux2_out;
+tiny8_word regfilemux_out;
+tiny8_opcode opcode;
+tiny8_reg rs;
+tiny8_reg rd;
+logic [1:0] delta2;
+logic [3:0] imm4;
 
 register pc
 (
     .clk,
     .load(load_pc),
-    .in(pc_in),
+    .in(pcmux_out),
     .out(pc_out)
 );
-
-tiny8_word ir_in;
-tiny8_word ir_out;
 
 register ir
 (
     .clk,
     .load(load_ir),
-    .in(ir_in),
+    .in(mem_rdata),
     .out(ir_out)
 );
-
-tiny8_word acc_in;
-tiny8_word acc_out;
 
 register acc
 (
     .clk,
     .load(load_acc),
-    .in(acc_in),
+    .in(acc_out + alu_out),
     .out(acc_out)
 );
-
-tiny8_word rs_in;
-tiny8_word rd_in;
-tiny8_word rs_out;
-tiny8_word rd_out;
 
 regfile regfile
 (
     .clk,
     .load1(load_rs),
     .load2(load_rd),
-    .in1(rs_in),
-    .in2(rd_in),
+    .in1(alu_out),
+    .in2(regfilemux_out),
     .r1(rs),
     .r2(rd),
     .out1(rs_out),
     .out2(rd_out)
 );
 
-tiny8_word alu_in1;
-tiny8_word alu_in2;
-tiny8_word alu_out;
-
 alu alu
 (
     .aluop,
-    .a(alu_in1),
-    .b(alu_in2),
+    .a(alumux1_out),
+    .b(alumux2_out),
     .f(alu_out)
+);
+
+mux2 pcmux
+(
+    .sel(pcmux_sel),
+    .a(pc_out + 1'b1),
+    .b(pc_out + imm4),
+    .f(pcmux_out)
+);
+
+
+mux4 addrmux
+(
+    .sel(addrmux_sel),
+    .a(pc_out),
+    .b(rs_out),
+    .c(rd_out),
+    .d(),
+    .f(addrmux_out)
+);
+
+mux2 alumux1
+(
+    .sel(alumux1_sel),
+    .a(rs_out),
+    .b(rd_out),
+    .f(alumux1_out)
+);
+
+mux2 alumux2
+(
+    .sel(alumux2_sel),
+    .a({6'd0, delta2}),
+    .b({4'd0, imm4}),
+    .f(alumux2_out)
+);
+
+mux2 regfilemux
+(
+    .sel(regfilemux_sel),
+    .a(alu_out),
+    .b(mem_rdata),
+    .f(regfilemux_out)
+);
+
+ir ir
+(
+    .clk,
+    .load(load_ir),
+    .in(mem_rdata),
+    .opcode,
+    .rs,
+    .rd,
+    .delta2,
+    .imm4
 );
 
 endmodule
---
+++ src/ir.sv
@@ -0,0 +1,29 @@
+import tiny8_types::*;
+
+module ir
+(
+    input clk,
+    input load,
+    input tiny8_word in,
+    output tiny8_opcode opcode,
+    output tiny8_reg rs,
+    output tiny8_reg rd,
+    output logic [1:0] delta2,
+    output logic [3:0] imm4
+);
+
+tiny8_word data;
+
+always_ff @(posedge clk)
+begin
+    if (load)
+        data = in;
+end
+
+assign opcode tiny8_opcode'(data[7:6]);
+assign rs = data[5:4];
+assign rd = data[3:2];
+assign delta2 = data[1:0];
+assign imm4 = data[3:0]
+
+endmodule
